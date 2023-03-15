7250c0569f5f6ddc6ee0b34021d9334edd1e5760
IsaacAndra <isaac.andra84@gmail.com>
2023-03-14 23:03:30 -0300

    :sparkles: feat: Validação do campo CARD NUMBER
    Foi adicionado ao campo CARD NUMBER, validação com regex e mínimo de dígitos

--- scripts/validaFormulario.js
+++ scripts/validaFormulario.js
@@ -1,3 +1,5 @@
+/*   Validação do campo NOME     */
+
 const nameInput = document.getElementById('card-name');
 const nameError = document.getElementById('name-error');
 
@@ -26,4 +28,37 @@ function validateName(nome) {
     }
     return '';
    
+}
+/*  ------------------   */
+
+/*   Validação do campo CARD NUMBER     */
+
+const cardNumberInput = document.getElementById('card-number');
+const cardNumberError = document.getElementById('card-number-error');
+
+cardNumberInput.addEventListener('blur', () => {
+    const number = cardNumberInput.value;
+    if (validateNumber(number)) {
+        cardNumberError.textContent = validateNumber(number);
+        cardNumberInput.classList.add('erro');
+    } else {
+        cardNumberError.textContent = '';
+        cardNumberInput.classList.remove('erro');
+    }
+})
+
+
+function validateNumber(number) {
+    const regex = /^[0-9\s]+$/;
+
+    if(!number) {
+        return 'Por favor, preencha o campo nome';
+    }
+    if(!regex.test(number)) {
+        return 'O campo nome deve conter apenas números e espaços';
+    }
+    if(number.length < 16) {
+        return 'O campo deve conter 16 dígitos';
+    }
+    return '';
 }
