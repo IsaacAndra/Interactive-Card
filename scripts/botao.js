d35632290c6b3ec6108cb61a2fffeeacf7acfb53
IsaacAndra <isaac.andra84@gmail.com>
2023-03-17 21:42:06 -0300

    :bulb: docs: Evento de click
    Foi adicionado ao evento de click do botao, uma verificaçao de formulario

--- scripts/botao.js
+++ scripts/botao.js
@@ -1,5 +1,6 @@
 const btn = document.getElementById('submit');
 const form = document.getElementById('formulario');
+const state = document.getElementById('final-state')
 
 btn.addEventListener('click', (e) => {
     const nome = nameInput.value;
@@ -9,7 +10,11 @@ btn.addEventListener('click', (e) => {
     e.preventDefault();
     if(validateName(nome) || validateNumber(number) || validateExp(expiration) || validateCvv(cvv)) {
         alert('You need to fill all the fields correctly');
+    } else if(number.length < 16) {
+        alert('The card number need to be 16 digits');
+        cardNumberInput.classList.add('erro');
     } else {
         form.style.display = 'none';
+        state.style.display = 'flex';
     }
 })
